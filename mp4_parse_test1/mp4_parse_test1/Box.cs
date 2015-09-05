4fa376b987e6f8756eb40462971aabe75ac2ef65
ragingo <save.the.world.crush@gmail.com>
2015-09-06 01:54:01 +0900

    掃除

--- mp4_parse_test1/mp4_parse_test1/Box.cs
+++ mp4_parse_test1/mp4_parse_test1/Box.cs
@@ -22,7 +22,7 @@ namespace mp4_parse_test1
 		Vmhd = ('v' << 24) | ('m' << 16) | ('h' << 8) | ('d' << 0),
 		Dinf = ('d' << 24) | ('i' << 16) | ('n' << 8) | ('f' << 0),
 		Dref = ('d' << 24) | ('r' << 16) | ('e' << 8) | ('f' << 0),
-		Url = ('u' << 24) | ('r' << 16) | ('l' << 8) | (' ' << 0),
+		Url  = ('u' << 24) | ('r' << 16) | ('l' << 8) | (' ' << 0),
 		Stbl = ('s' << 24) | ('t' << 16) | ('b' << 8) | ('l' << 0),
 		Smhd = ('s' << 24) | ('m' << 16) | ('h' << 8) | ('d' << 0),
 		Stsd = ('s' << 24) | ('t' << 16) | ('s' << 8) | ('d' << 0),
@@ -42,53 +42,58 @@ namespace mp4_parse_test1
 		Free = ('f' << 24) | ('r' << 16) | ('e' << 8) | ('e' << 0),
 	}
 
-	public class Box
+	public static class HandlerTypes
 	{
-		public UInt32 Size { get; set; }
-		public BoxType Type { get; set; }
+		public const string Sound = "soun";
+		public const string Video = "vide";
+	}
 
-		public Box(BoxType type)
+	class BoxNode
+	{
+		public long Offset { get; set; }
+		public uint Size { get; set; }
+		public BoxType Type { get; set; }
+		public BoxNode Parent { get; set; }
+		public List<BoxNode> Children { get; private set; }
+		public int Level { get; set; }
+		public bool IsRoot { get { return Level == 0; } }
+		public BoxNode()
 		{
-			Type = type;
+			Children = new List<BoxNode>();
+		}
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append(StringUtils.FromBinary((uint)Type));
+			sb.AppendFormat(" {{ offset:{0:#,#}, size:{1:#,#} }}", Offset, Size);
+			return sb.ToString();
 		}
 
-		public string GetName()
+		public T As<T>()
+			where T : BoxNode, new()
 		{
-			return StringUtils.FromBinary((UInt32)Type);
+			var newNode = new T();
+			newNode.Offset = Offset;
+			newNode.Size = Size;
+			newNode.Type = Type;
+			newNode.Parent = Parent;
+			newNode.Children.AddRange(Children);
+			newNode.Level = Level;
+			return newNode;
 		}
 	}
 
-	public class FullBox : Box
+	class FullBoxNode : BoxNode
 	{
 		public byte Version { get; set; }
 		public UInt32 Flags { get; set; }
 
-		public FullBox(BoxType type, byte version, UInt32 flags) : base(type)
-		{
-			Version = version;
-			Flags = flags;
-		}
-	}
-
-	public class FileTypeBox : Box
-	{
-		public UInt32 MajorBrand { get; set; }
-		public UInt32 MinorVersion { get; set; }
-		public UInt32 CompatibleBrands { get; set; }
-
-		public FileTypeBox() : base(BoxType.Ftyp)
+		public FullBoxNode()
 		{
 		}
 	}
 
-	public class MoovBox : Box
-	{
-		public MoovBox() : base(BoxType.Moov)
-		{
-		}
-	}
-
-	public class MvhdBox : FullBox
+	class MvhdBoxNode : FullBoxNode
 	{
 		public DateTime CreationTime { get; set; }
 		public DateTime ModificationTime { get; set; }
@@ -98,22 +103,54 @@ namespace mp4_parse_test1
 		public Single Volume { get; set; }
 		public UInt32 NextTrackId { get; set; }
 
-		public MvhdBox(byte version, UInt32 flags) : base(BoxType.Mvhd, version, flags)
+		public MvhdBoxNode()
 		{
 		}
 	}
 
-	public class TrakBox : Box
+	class StsdBoxNode : BoxNode
 	{
-		public TrakBox() : base(BoxType.Trak)
+		public uint SampleEntries { get; set; }
+
+		public StsdBoxNode()
 		{
 		}
 	}
 
-	public class TkhdBox : FullBox
+	class SampleEntryNode : BoxNode
 	{
-		public TkhdBox(byte version, UInt32 flags) : base(BoxType.Tkhd, version, flags)
+		public byte[] Reserved { get; private set; }
+		public UInt16 DataReferenceIndex { get; set; }
+
+		public SampleEntryNode()
 		{
+			Reserved = new byte[6];
+		}
+	}
+
+	class VisualSampleEntryNode : SampleEntryNode
+	{
+		public UInt16 PreDefined { get; set; }
+		public UInt16 Reserved2 { get; set; }
+		public UInt32[] PreDefined2 { get; set; }
+		public UInt16 Width { get; set; }
+		public UInt16 Height { get; set; }
+		public UInt32 HorizontalResolution { get; set; }
+		public UInt32 VerticalResolution { get; set; }
+		public UInt32 Reserved3 { get; set; }
+		public UInt16 FrameCount { get; set; }
+		public string CompressorName { get; set; }
+		public UInt16 Depth { get; set; }
+		public Int32 PreDefined3 { get; set; }
+
+		public VisualSampleEntryNode()
+		{
+			PreDefined2 = new UInt32[3];
+			HorizontalResolution = 0x00480000 >> 16;
+			VerticalResolution = 0x00480000 >> 16;
+			FrameCount = 1;
+			Depth = 0x0018 >> 8;
+			PreDefined3 = -1;
 		}
 	}
 }
---
+++ mp4_parse_test1/mp4_parse_test1/BoxParser.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mp4_parse_test1
+{
+	class BoxParser
+	{
+		public List<BoxNode> GetBoxes(BinaryReader2 reader)
+		{
+			return GetBoxes(reader, null);
+		}
+
+		private List<BoxNode> GetBoxes(BinaryReader2 reader, BoxNode parent)
+		{
+			var nodes = new List<BoxNode>();
+
+			while (reader.BaseStream.Position < reader.BaseStream.Length)
+			{
+				UInt32 boxSize = reader.ReadUInt32();
+				BoxType boxType = (BoxType)reader.ReadUInt32();
+
+				var sibling = new BoxNode();
+				sibling.Offset = reader.BaseStream.Position - 4 * 2;
+				sibling.Size = boxSize;
+				sibling.Type = boxType;
+				sibling.Parent = parent;
+				nodes.Add(sibling);
+
+				switch (boxType)
+				{
+				case BoxType.Moov:
+				case BoxType.Trak:
+				case BoxType.Mdia:
+				case BoxType.Minf:
+				case BoxType.Dinf:
+				case BoxType.Stbl:
+				case BoxType.Udta:
+					sibling.Children.AddRange(GetBoxes(reader, sibling));
+					break;
+				case BoxType.Mvhd:
+					ParseMvhd(reader, sibling);
+					break;
+				case BoxType.Dref:
+					ParseDref(reader, sibling);
+					break;
+				case BoxType.Stsd:
+					ParseStsd(reader, sibling);
+					break;
+				case BoxType.Avc1:
+					ParseAvc1(reader, sibling);
+					break;
+				case BoxType.Mp4a:
+					ParseMp4a(reader, sibling);
+					break;
+				default:
+					reader.BaseStream.Seek((boxSize - 4 * 2), SeekOrigin.Current);
+					break;
+				}
+
+				if (parent != null &&
+					parent.Offset + parent.Size == sibling.Offset + sibling.Size)
+				{
+					return nodes;
+				}
+			}
+
+			return nodes;
+		}
+
+		// Movie Header Box
+		private void ParseMvhd(BinaryReader2 reader, BoxNode sibling)
+		{
+			var newSibling = sibling.As<MvhdBoxNode>();
+			newSibling.Version = reader.ReadByte();
+			newSibling.Flags = reader.ReadUInt24();
+			if (newSibling.Version == 1)
+			{
+				newSibling.CreationTime = (new DateTime(1904, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddSeconds(reader.ReadUInt64());
+				newSibling.ModificationTime = (new DateTime(1904, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddSeconds(reader.ReadUInt64());
+				newSibling.TimeScale = reader.ReadUInt32();
+				newSibling.Duration = reader.ReadUInt64();
+			}
+			else
+			{
+				newSibling.CreationTime = (new DateTime(1904, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddSeconds(reader.ReadUInt32());
+				newSibling.ModificationTime = (new DateTime(1904, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddSeconds(reader.ReadUInt32());
+				newSibling.TimeScale = reader.ReadUInt32();
+				newSibling.Duration = reader.ReadUInt32();
+			}
+			newSibling.Rate = (Double)(reader.ReadUInt32() >> 16);
+			newSibling.Volume = (Single)(reader.ReadInt16() >> 8);
+			reader.ReadBytes(2); // reserved1: bit(16)
+			reader.ReadBytes(4 * 2); // reserved2: unsigned int(32) [2]
+			reader.ReadBytes(4 * 9); // matrix: template int(32) [9]
+			reader.ReadBytes(4 * 6); // pre_defined: bit(32) [6]
+			newSibling.NextTrackId = reader.ReadUInt32();
+		}
+
+		// Data Reference Box
+		private void ParseDref(BinaryReader2 reader, BoxNode sibling)
+		{
+			reader.BaseStream.Seek(4, SeekOrigin.Current); // FullBox
+			reader.ReadUInt32();// entries
+			sibling.Children.AddRange(GetBoxes(reader, sibling));
+		}
+
+		// Sample Description Box
+		private void ParseStsd(BinaryReader2 reader, BoxNode sibling)
+		{
+			reader.BaseStream.Seek(4, SeekOrigin.Current); // FullBox
+			var newSibling = sibling.As<StsdBoxNode>();
+			newSibling.SampleEntries = reader.ReadUInt32();
+			sibling.Children.AddRange(GetBoxes(reader, newSibling));
+		}
+
+		// 8.5.1. p32 AudioSampleEntry
+		private void ParseMp4a(BinaryReader2 reader, BoxNode sibling)
+		{
+			reader.BaseStream.Seek(6 + 2, SeekOrigin.Current); // SampleEntry
+			reader.BaseStream.Seek(4 * 2 + 2 + 2 + 2 + 2 + 4, SeekOrigin.Current);
+			sibling.Children.AddRange(GetBoxes(reader, sibling));
+		}
+
+		// 8.5.1. p31 VisualSampleEntry
+		private void ParseAvc1(BinaryReader2 reader, BoxNode sibling)
+		{
+			var newSibling = sibling.As<VisualSampleEntryNode>();
+			reader.BaseStream.Seek(6, SeekOrigin.Current); // reserved
+			newSibling.DataReferenceIndex = reader.ReadUInt16();
+			reader.BaseStream.Seek(2 + 2 + 4 * 3, SeekOrigin.Current); // pre_defined + reserved + predefined
+			newSibling.Width = reader.ReadUInt16();
+			newSibling.Height = reader.ReadUInt16();
+			newSibling.HorizontalResolution = reader.ReadUInt32() >> 16;
+			newSibling.VerticalResolution = reader.ReadUInt32() >> 16;
+			reader.BaseStream.Seek(4, SeekOrigin.Current); // reserved
+			newSibling.FrameCount = (ushort)(reader.ReadUInt16() >> 8);
+			newSibling.CompressorName = Encoding.ASCII.GetString(reader.ReadBytes(32));
+			newSibling.Depth = (ushort)(reader.ReadUInt16() >> 8);
+			reader.BaseStream.Seek(2, SeekOrigin.Current); // pre_defined
+			sibling.Children.AddRange(GetBoxes(reader, newSibling));
+		}
+	}
+}
